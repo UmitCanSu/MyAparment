60231f2eac81556022e137ea5184735ed40d7ce0
UmitCanSu <umitsu255@gmail.com>
2024-01-15 20:54:05 +0300

    add main fragment

---
+++ app/src/main/java/com/example/myapartments/presentation/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.myapartments.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapartments.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
