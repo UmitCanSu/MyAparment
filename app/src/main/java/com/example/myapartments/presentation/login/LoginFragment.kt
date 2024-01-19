2e79691041d0e3baacfc5b0a4df36c0dd6468fb1
UmitCanSu <umitsu255@gmail.com>
2024-01-19 20:31:33 +0300

    add login fragment

--- app/src/main/java/com/example/myapartments/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/myapartments/presentation/login/LoginFragment.kt
@@ -1,15 +1,14 @@
-package com.example.myapartments.presentation
+package com.example.myapartments.presentation.login
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myapartments.databinding.FragmentMainBinding
+import com.example.myapartments.R
 
-class MainFragment : Fragment() {
 
-    private lateinit var binding: FragmentMainBinding
+class LoginFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,9 +16,9 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentMainBinding.inflate(layoutInflater)
-        return binding.root
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
 }
